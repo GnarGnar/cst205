f7e92fc7ad2e54408f7a9eb66b70c72aed12c292
GnarGnar <kburke@csumb.edu>
2016-08-31 10:40:27 -0700

    Here we go

---
+++ fizzbuzz.py
@@ -0,0 +1,20 @@
+number = int =0
+
+for number in range(0,100):
+	
+	if(number%3==0 and number%5==0):
+		print(number)
+		print("FizzBuZZ")
+	
+	elif(number%3==0):
+		print(number)
+		print("Fizz")
+	elif(number%5 ==0):
+		print(number)
+		print("Buzz")
+	
+	
+	
+	
+	
+	
