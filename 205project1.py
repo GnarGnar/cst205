75edd44f86163c0481ffcbf41b99b8e20fda22ff
GnarGnar <kburke@csumb.edu>
2016-09-12 11:05:14 -0700

    Much Faster

--- 205project1.py
+++ 205project1.py
@@ -2,6 +2,7 @@
 Abstract: Goes through 9 images finding the median value of its pixels and putting it in a new images
 Author: Kieran Burke
 Date:9/3/16
+Github: https://github.com/GnarGnar/cst205
 
 Takes a while to run but it works
 """
@@ -10,23 +11,14 @@ from PIL import Image
 import statistics
 
 myimage1= Image.open("1.png")
-px1 = myimage1.load()
 myimage2= Image.open("2.png")
-px2 = myimage2.load()
 myimage3= Image.open("3.png")
-px3 = myimage3.load()
 myimage4= Image.open("4.png")
-px4 = myimage4.load()
 myimage5= Image.open("5.png")
-px5 = myimage5.load()
 myimage6= Image.open("6.png")
-px6 = myimage6.load()
 myimage7= Image.open("7.png")
-px7 = myimage7.load()
 myimage8= Image.open("8.png")
-px8 = myimage8.load()
 myimage9= Image.open("9.png")
-px9 = myimage9.load()
 size = myimage1.size
 width, height = myimage1.size
 myimage10 = Image.new(myimage1.mode,myimage1.size);
@@ -60,11 +52,9 @@ for x in range(0,len(pixel_values1)-1):
 	pixels.append(pixel_values8[x])
 	pixels.append(pixel_values9[x])
 	pixels.sort()
-	newImagePixels.append(pixels[4])
+	newImagePixels.append(pixels[5])
 	for x in range(0,9):
 		pixels.pop()
-	print("Working on it...")
-	print("Working on it..")
 	
 myimage10.putdata(newImagePixels)
 myimage10.show()
